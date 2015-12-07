5aeaa8b0b936b6778d9b337cf7e577504354d417
Miguel Gonzalez <miguel-gonzalez@gmx.de>
2015-12-07 10:57:58 +0100

    Add initial demo project

---
+++ demo/src/main/java/nl/fontys/demo/Application.java
@@ -0,0 +1,14 @@
+package nl.fontys.demo;
+
+/**
+ * Application to run the demo
+ *
+ * @author Miguel Gonzalez
+ * @author Jan Kerkenhoff
+ */
+public class Application {
+
+    public static void main(String[] args) {
+
+    }
+}
